7ca11d014590f740f1408508058cf1c09670e573
BothSann <boththann76@gmail.com>
2025-07-28 18:14:26 +0700

    add AlertTextField component and update build.gradle.kts for project configuration

---
+++ feature/profile/src/commonMain/kotlin/com/nutritionwarehouse/profile/component/AlertTextField.kt
@@ -0,0 +1,66 @@
+package com.nutritionwarehouse.profile.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.nutritionwarehouse.shared.BorderIdle
+import com.nutritionwarehouse.shared.FontSize
+import com.nutritionwarehouse.shared.SurfaceLighter
+import com.nutritionwarehouse.shared.TextPrimary
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun AlertTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    icon: DrawableResource? = null,
+    onClick: () -> Unit,
+) {
+    Row (
+        modifier = modifier
+            .background(SurfaceLighter)
+            .border(
+                width = 1.dp,
+                color = BorderIdle,
+                shape = RoundedCornerShape(size = 6.dp)
+            )
+            .clip(RoundedCornerShape(size = 6.dp))
+            .clickable{onClick()}
+            .padding(
+                vertical = 20.dp,
+                horizontal = 16.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (icon != null) {
+            Image(
+                modifier = Modifier
+                    .size(14.dp),
+                painter = painterResource(icon),
+                contentDescription = "Text field icon",
+            )
+            Spacer(
+                modifier = Modifier
+                    .size(8.dp)
+            )
+            Text(
+                text = text,
+                fontSize = FontSize.REGULAR,
+                color = TextPrimary
+            )
+        }
+    }
+}
